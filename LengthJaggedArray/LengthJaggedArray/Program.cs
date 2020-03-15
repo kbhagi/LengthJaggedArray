c046f4b5a7f37a1c11e96cc6ed5fbe5a152c87c4
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 17:52:46 +0530

    Add files via upload

---
+++ LengthJaggedArray/LengthJaggedArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LengthJaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[][] numbers = new byte[5][];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new byte[i + 3];
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, numbers[i].Length);
+            }
+            Console.Read();
+        }
+    }
+}
